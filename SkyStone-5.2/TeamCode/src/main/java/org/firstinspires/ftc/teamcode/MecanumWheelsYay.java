0b0267c9f52db8ad8d6375191e5d1de653ca6754
Amanda <43766405+RoboticPanda07@users.noreply.github.com>
2019-11-11 20:27:12 -0500

    Update MecanumWheelsYay.java

--- SkyStone-5.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumWheelsYay.java
+++ SkyStone-5.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumWheelsYay.java
@@ -1,4 +1,3 @@
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.lynx.LynxModule;
@@ -8,8 +7,9 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-
+import com.qualcomm.robotcore.util.Range;
 import java.util.List;
+
 
 @TeleOp(name = "Mecanum Wheels YAY lol")
 public class MecanumWheelsYay extends OpMode {
@@ -22,10 +22,13 @@ public class MecanumWheelsYay extends OpMode {
         robot.rightRear = hardwareMap.get(DcMotor.class, robot.rrName);
         robot.leftFront = hardwareMap.get(DcMotor.class, robot.lfName);
         robot.rightFront = hardwareMap.get(DcMotor.class, robot.rfName);
+
+
+        robot.linearSlide = hardwareMap.get(DcMotor.class,robot.lsName);
+
         robot.arm = hardwareMap.get(DcMotor.class, robot.armName);
         robot.claw = hardwareMap.servo.get(robot.clawName);
-       
-
+        robot.foundation = hardwareMap.servo.get(robot.fName);
         robot.rightRear.setDirection(DcMotor.Direction.REVERSE);
         robot.rightFront.setDirection(DcMotor.Direction.REVERSE);
 
@@ -51,12 +54,57 @@ public class MecanumWheelsYay extends OpMode {
         robot.leftRear.setPower(v3);
         robot.rightRear.setPower(v4);
 
+
+  /*     double leftX = gamepad1.left_stick_x;
+       double leftY = -gamepad1.left_stick_y; //Gamepad Moving up is giving -ve values
+       double rightX = gamepad1.right_stick_x;
+       double rightY = -gamepad1.right_stick_y; //Gamepad Moving up is giving -ve values
+
+       double[] tempArray = AppUtil.getVectorArrays(leftX, leftY, rightX);
+       double v1 = tempArray[0];
+       double v2 = tempArray[1];
+       double v3 = tempArray[2];
+       double v4 = tempArray[3];
+
+       //It is necessary to set one side of motor to REVERSE
+       //Motor is facing other directions so reverse it.-- Kadhir
+       v1 = -v1;
+       v3 = -v3;
+
+       telemetry.addData("Status", i + " x " + leftX + " y " + leftY + " z " + rightX + " v1>> " + v1);
+       telemetry.addData("Status", i + ".v2>> " + v2);
+       telemetry.addData("Status", i + ".v3>> " + v3);
+       telemetry.addData("Status", i + ".v4>> " + v4);
+       telemetry.update();
+
+
+       leftFront.setPower(v1);
+       rightFront.setPower(v2);
+       leftRear.setPower(v3);
+       rightRear.setPower(v4);
+       i++;
+*/
+
+        robot.arm.setPower(-gamepad2.right_stick_y);
+        robot.linearSlide.setPower(-gamepad2.left_stick_y);
+
         robot.arm.setPower(-gamepad2.right_stick_y);
 
+
+//open and close claw
         if (gamepad2.a) {
             robot.claw.setPosition(robot.CLAW_OPEN);
         } else if (gamepad2.b) {
             robot.claw.setPosition(robot.CLAW_CLOSE);
+        }
+
+
+
+//Foundation open and close
+        if (gamepad2.left_bumper) {
+            robot.foundation.setPosition(robot.FOUNDATION_DOWN);
+        } else if (gamepad2.right_bumper) {
+            robot.foundation.setPosition(robot.FOUNDATION_UP);
         }
 
 
