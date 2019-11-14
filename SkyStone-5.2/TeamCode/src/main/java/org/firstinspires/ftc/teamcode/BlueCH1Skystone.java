2da4fbf6f49507f3a5cb92cbe39e1a9603e8675c
Amanda <43766405+RoboticPanda07@users.noreply.github.com>
2019-11-13 19:09:26 -0500

    Reversed direction for other side of field (blue)

---
+++ SkyStone-5.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueCH1Skystone.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.vuforia.CameraDevice;
+@Autonomous(name = "Blue Channel 1 Skystone", group = "autonomous")
+public class BlueCH1Skystone extends CBAutonomousBase {
+    String inputColor = "red";
+
+    @Override
+    public void runOpMode() {
+
+        initialization();
+        waitForStart();
+
+        encoderDrive("MB", 25, 0.7, 0,false);
+        sleep(1000);
+        util.updateStatus(">", "Searching for Skystone...");
+
+        CameraDevice.getInstance().setFlashTorchMode(true);
+
+        encoderDrive("ML", 36, 0.2, 0,true);
+        util.updateStatus(">", "Skystone found! navigating...");
+
+        CameraDevice.getInstance().setFlashTorchMode(false);
+
+        sleep(1000);
+
+        if (vcb.targetVisible()) {
+            vuforiaNavigate(0.0, "red");
+        }
+        sleep(2000);
+
+        //TBD
+        // pick up skystone
+
+        util.updateStatus(">", "Skystone Delivered!");
+
+        encoderDrive("MR", 72, 0.8, 0,false, true);
+        sleep(1000);
+        encoderDrive("MR", 24, 0.5, 0,false);
+        sleep(1000);
+        encoderDrive("ML", 24, 0.5, 0,false, true);
+
+        //TBD
+        //drop the skystone
+
+
+    }
+
+
+
+}
